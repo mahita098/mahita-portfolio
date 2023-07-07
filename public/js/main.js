2679d9c3b49f75335c53ce00005bc273ee3a61b9
mahita098 <mahita098@gmail.com>
2023-07-07 12:27:12 +0400

    move main.js to public

---
+++ public/js/main.js
@@ -0,0 +1,4 @@
+document.querySelector(".hamburger1").addEventListener("click", function () {
+  this.classList.toggle("active");
+  document.querySelector(".menu-item1").classList.toggle("active");
+});
